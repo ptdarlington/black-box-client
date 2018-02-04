0e67d39c848fb9adf30021a68ba703b97cf2092c
Paige Darlington <paigetaylordarlington@gmail.com>
2018-02-03 21:22:37 -0800

    Fetch and display data from server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+    this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+    this.renderBoxes = this.renderBoxes.bind(this);
+  }
+
+  loadCommentsFromServer() {
+    axios.get('http://localhost:3001/api/boxes').then(res => {
+      this.setState({ data: res.data });
+    });
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+  }
+
+  renderBoxes() {
+    return this.state.data.map(function(box) {
+      return <tr key={box}><td>{box.name}</td><td>{box.createdAt}</td></tr>;
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,6 +38,19 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Created</th>
+              </tr>
+            </thead>
+            <tbody>
+              { this.renderBoxes() }
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
